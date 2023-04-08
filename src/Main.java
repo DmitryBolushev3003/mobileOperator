99841f61f3a7e7a1a0a114ed07145a7f52366af0
Dima Bolushev <bdima3003@mail.ru>
2023-04-08 11:30:56 +0300

    OKprogram

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-       
+       int initialAmount = 20;
+       int deposit = 1080;
+       int bonus = deposit / 100;
+       int balans = initialAmount + deposit + bonus;
+
+       if (deposit > 1000) {
+           System.out.println("Спасибо за пополнение счета на сумму " + deposit +  ", вам начислено бонусов " + bonus);
+       } else {
+           System.out.println("Спасибо за пополнение счета, на сумму " + deposit + " к сожалению сумма зачислений не предпологает начисления бонусов(" );
+       }
+        System.out.println("Ваш баланс составляет " + balans);
     }
 }
